eb776fb877ea131b6622f0e80f9de952f5a7c4db
saurabhs <saurabhpshirsath@gmail.com>
2021-08-26 20:41:49 +0530

    [saurabh]Add:Create Table In DataBase

--- Employee_PayRoll.sql
+++ Employee_PayRoll.sql
@@ -1,3 +1,10 @@
 create database EmployeePayRoll
 
 use EmployeePayRoll
+
+Create Table employee_payroll (
+Id int not null identity (1,1) primary key,
+Name varchar(20),
+Salary varchar(20),
+StartDate varchar(20)
+)
