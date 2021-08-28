759939c162499eff7addef9b05611969fbefd206
saurabhs <saurabhpshirsath@gmail.com>
2021-08-28 12:16:13 +0530

    [saurabh]Add:Retrive Particular Data From Table In DataBase

--- Employee_PayRoll.sql
+++ Employee_PayRoll.sql
@@ -8,10 +8,17 @@ Name varchar(20),
 Salary varchar(20),
 StartDate varchar(20)
 )
-insert into employee_payroll (Name,Salary,StartDate)values ('shree','30000','26-08-2021')
-insert into employee_payroll (Name,Salary,StartDate)values ('sai','40000','26-08-2021')
-insert into employee_payroll (Name,Salary,StartDate)values ('shree','50000','26-08-2021')
+
+insert into employee_payroll (Name,Salary,StartDate)values ('shree','30000','2016-01-01')
+insert into employee_payroll (Name,Salary,StartDate)values ('Bills','30000','2010-01-01')
+insert into employee_payroll (Name,Salary,StartDate)values ('sai','30000','2013-01-10')
 
 select * from employee_payroll
+
+SELECT *
+FROM employee_payroll
+WHERE Name='Bills' AND StartDate BETWEEN CAST('2010-01-01' AS DATE ) AND GETDATE();
+
+
 
 
